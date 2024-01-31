b6517a34f4a55597b34cd082c2113a3d6cc18072
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-01-31 13:36:53 -0300

    feat: implements database layer for category and product

---
+++ go-api/internal/database/category_db.go
@@ -0,0 +1,81 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/saboyagustavo/go-ecommerce/internal/entity"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{db: db}
+}
+
+func (catDB *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := catDB.db.Query(`
+		SELECT 
+			id, 
+			name
+		FROM categories
+	`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categories []*entity.Category
+
+	for rows.Next() {
+		var category entity.Category
+
+		err := rows.Scan(&category.ID, &category.Name)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, &category)
+	}
+
+	return categories, nil
+}
+
+func (catDB *CategoryDB) GetCategory(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := catDB.db.QueryRow(`
+		SELECT 
+			id, 
+			name
+		FROM categories
+		WHERE id = ?
+	`, id,
+	).Scan(&category.ID, &category.Name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &category, nil
+}
+
+func (catDB *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := catDB.db.Exec(`
+		INSERT INTO categories (
+			id, name
+		) VALUES (
+			?, ?
+		)
+	`,
+		category.ID,
+		category.Name,
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return category.ID, nil
+}
---
+++ go-api/internal/database/product_db.go
@@ -0,0 +1,158 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/saboyagustavo/go-ecommerce/internal/entity"
+)
+
+type ProductDB struct {
+	db *sql.DB
+}
+
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (proDB *ProductDB) GetProduct(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := proDB.db.QueryRow(`
+		SELECT
+			id,
+			name,
+			description,
+			category_id,
+			image_url,
+			price
+		FROM products
+		WHERE id = ?
+	`, id,
+	).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Descritption,
+		&product.CategoryID,
+		&product.ImageURL,
+		&product.Price,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
+
+func (proDB *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows, err := proDB.db.Query(`
+		SELECT
+			id,
+			name,
+			description,
+			category_id,
+			image_url,
+			price
+		FROM products
+	`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var products []*entity.Product
+
+	for rows.Next() {
+		var product entity.Product
+
+		err := rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.Descritption,
+			&product.CategoryID,
+			&product.ImageURL,
+			&product.Price,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, &product)
+	}
+
+	return products, nil
+}
+
+func (proDB *ProductDB) GetProductsByCategoryID(categoryID string) ([]*entity.Product, error) {
+	rows, err := proDB.db.Query(`
+		SELECT
+			id,
+			name,
+			description,
+			category_id,
+			image_url,
+			price
+		FROM products
+		WHERE category_id = ?
+	`, categoryID,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var products []*entity.Product
+
+	for rows.Next() {
+		var product entity.Product
+
+		err := rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.Descritption,
+			&product.CategoryID,
+			&product.ImageURL,
+			&product.Price,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, &product)
+	}
+
+	return products, nil
+}
+
+func (proDB *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
+	_, err := proDB.db.Exec(`
+		INSERT INTO products (
+			id,
+			name,
+			description,
+			category_id,
+			image_url,
+			price
+		) VALUES (
+			?, ?, ?, ?, ?, ?
+		)
+	`,
+		product.ID,
+		product.Name,
+		product.Descritption,
+		product.CategoryID,
+		product.ImageURL,
+		product.Price,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
