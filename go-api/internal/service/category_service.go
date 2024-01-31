a4c8a5dd8997644cdee31abeb9052b597abf5a46
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-01-31 14:47:43 -0300

    feat: implements service layer for category and product

---
+++ go-api/internal/service/category_service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/saboyagustavo/go-ecommerce/internal/database"
+	"github.com/saboyagustavo/go-ecommerce/internal/entity"
+)
+
+type CategoryService struct {
+	CategoryDB database.CategoryDB
+}
+
+func NewCategoryService(categoryDB database.CategoryDB) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (cs *CategoryService) GetCategories() ([]*entity.Category, error) {
+	categories, err := cs.CategoryDB.GetCategories()
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (cs *CategoryService) GetCategory(id string) (*entity.Category, error) {
+	category, err := cs.CategoryDB.GetCategory(id)
+	if err != nil {
+		return nil, err
+	}
+	return category, err
+}
+
+func (cs *CategoryService) CreateCategory(name string) (string, error) {
+	category := entity.NewCategory(name)
+	categoryID, err := cs.CategoryDB.CreateCategory(category)
+	if err != nil {
+		return "", err
+	}
+	return categoryID, nil
+}
---
+++ go-api/internal/service/product_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"github.com/saboyagustavo/go-ecommerce/internal/database"
+	"github.com/saboyagustavo/go-ecommerce/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDB
+}
+
+func NewProductService(productDB database.ProductDB) *ProductService {
+	return &ProductService{ProductDB: productDB}
+}
+
+func (ps *ProductService) GetProduct(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProduct(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+func (ps *ProductService) GetProductsByCategoryId(categoryID string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductsByCategoryID(categoryID)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) CreateProduct(
+	name, descritption, categoryID, imageURL string,
+	price float64,
+) (*entity.Product, error) {
+	product := entity.NewProduct(name, descritption, categoryID, imageURL, price)
+	_, err := ps.ProductDB.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
