7187337ff15b6159f7fa85a9acc3cf58db178c84
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-01-31 12:46:57 -0300

    feat: implements entities - product entity

--- go-api/internal/entity/entity.go
+++ go-api/internal/entity/entity.go
@@ -13,3 +13,26 @@ func NewCategory(name string) *Category {
 		Name: name,
 	}
 }
+
+type Product struct {
+	ID           string
+	Name         string
+	Descritption string
+	CategoryID   string
+	ImageURL     string
+	Price        float64
+}
+
+func NewProduct(
+	name, descritption, categoryID, imageURL string,
+	price float64,
+) *Product {
+	return &Product{
+		ID:           uuid.New().String(),
+		Name:         name,
+		Descritption: descritption,
+		CategoryID:   categoryID,
+		ImageURL:     imageURL,
+		Price:        price,
+	}
+}
