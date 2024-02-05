bb60ae84e5a07e581cb2bb7bfc730318b7dde2ca
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-02-05 13:33:12 -0300

    chore: bootstraping a nextjs app with typescript and material ui

---
+++ nextjs-frontend/src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { Box, ThemeProvider } from '@mui/material';
+import theme from '@/theme';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Code Commerce',
+	description: 'A Full Cycle Shop',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
+					<ThemeProvider theme={theme}>
+						<Box
+							component='main'
+							sx={{
+								flexGrow: 1,
+								bgcolor: 'background.default',
+								mt: ['122px', '135px', '146px'],
+								p: 3,
+							}}
+						>
+							{children}
+						</Box>
+					</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
+		</html>
+	);
+}
---
+++ nextjs-frontend/src/app/pagina1/page.tsx
@@ -0,0 +1,3 @@
+export default function Pagina1() {
+  return <h1>Hello World</h1>;
+}
