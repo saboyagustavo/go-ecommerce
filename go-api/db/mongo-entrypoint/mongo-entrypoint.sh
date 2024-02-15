b1ac147da7b25077b64ab816ca54b3ae4e7545ac
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-02-14 21:00:06 -0300

    chore: automatically seeds mongodb with image BLOB

--- go-api/db/mongo-entrypoint/mongo-entrypoint.sh
+++ go-api/db/mongo-entrypoint/mongo-entrypoint.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+echo "Executing MongoDB seed script..."
+mongoimport --authenticationDatabase admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD -d $MONGO_DB_NAME --collection images --type json --jsonArray --file /docker-entrypoint-initdb.d/init.json
+echo "Mongo database $MONGO_DB_NAME seeded successfully!"
+
 echo "Creating mongo users..."
 mongosh --authenticationDatabase admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD admin --eval "db.createUser({user: '$MONGO_USERNAME', pwd: '$MONGO_PASSWORD', roles: [{role: 'readWrite', db: '$MONGO_DB_NAME'}]});"
-echo "Mongo users created successfully."
+echo "Mongo users created successfully!"
